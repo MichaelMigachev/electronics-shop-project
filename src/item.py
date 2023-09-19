fbd52efe7b17e0b07386284a614b1bebef50a41a
MichaelMigachev <migachev21@gmail.com>
2023-09-19 19:40:06 +0300

    выполнил homework-2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,4 @@
+import csv
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +14,10 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        Item.all.append(self)
+        # Item.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -32,3 +33,29 @@ class Item:
         """
         self.price *= self.pay_rate
 
+    @classmethod
+    def instantiate_from_csv(cls, file_name):
+        """Создание метода класса для открытия файла csv и добавления экземпляров в новый
+           список"""
+        cls.all = []
+        with open(file_name) as f:
+            reader = csv.DictReader(f)
+            for r in reader:
+                cls.all.append(cls(r["name"], r["price"], r["quantity"]))
+
+    @staticmethod
+    def string_to_number(string_num):
+        """Создание статического метода для возврата числа из строки"""
+        if string_num.isdigit():
+            return int(string_num)
+        return float(string_num) // 1
+
+    @property
+    def name(self):
+        """Class name getter"""
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        """Class name setter"""
+        self.__name = new_name
--- tests/test_item.py
+++ tests/test_item.py
@@ -4,17 +4,26 @@ import pytest
 from src.item import Item
 
 def test_Item():
+    """Тестирование полей класса Item"""
     item = Item("tel", 5000, 2)
     assert item.name == "tel"
     assert item.price == 5000
     assert item.quantity == 2
 
 def test_calculate_total_price():
+    """Тестирование метода расчета количества товара"""
     item = Item("tel", 5000, 2)
     assert item.calculate_total_price() == 10000
 
 def test_apply_discount():
+    """Тестирование метода применения скидки возвращающий None"""
     item = Item("tel", 5000, 2)
     Item.pay_rate = 0.5
     item.apply_discount()
     assert item.price == 2500
+
+def test_Item_string_to_number():
+    """Тестирование @staticmethod string_to_number(...)"""
+    assert Item.string_to_number("8") == 8
+    assert Item.string_to_number("88.8") == 88
+
