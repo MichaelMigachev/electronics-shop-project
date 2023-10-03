a6744c55826f167c7f677e34b78e275bd9e1b806
MichaelMigachev <migachev21@gmail.com>
2023-10-03 06:29:57 +0300

    добавил test_keyboard.py к homework-5

---
+++ tests/test_keyboard.py
@@ -0,0 +1,19 @@
+from src.keyboard import Keyboard
+
+
+def test_keyboard():
+    kb = Keyboard('Dark Project KD87A', 9600, 5)
+    assert str(kb) == "Dark Project KD87A"
+
+    assert kb.name == "Dark Project KD87A"
+    assert kb.price == 9600
+    assert kb.quantity == 5
+
+    assert str(kb.language) == "EN"
+
+    kb.change_lang()
+    assert str(kb.language) == "RU"
+
+    # Сделали EN -> RU -> EN
+    kb.change_lang()
+    assert str(kb.language) == "EN"
